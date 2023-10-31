ea19c66a72cce48be0ad9483f858d6f4896e6397
antoniorr7 <146135540+antoniorr7@users.noreply.github.com>
2023-10-31 10:57:07 +0100

    Update config_db.php

--- Procesos/config_db.php
+++ Procesos/config_db.php
@@ -1,5 +1,5 @@
 <?php
-define('HOST', '16.2daw.esvirgua.com');   // Dirección del servidor MySQL
-define('USER', 'user2daw_16');    // Nombre de usuario de MySQL
-define('PASSWORD', 'CuW^nSP1*^$('); // Contraseña de MySQL
-define('DATABASE', 'user2daw_BD2-16'); // Nombre de la base de datos
+define('HOST', 'localhost');   // Dirección del servidor MySQL
+define('USER', 'root');    // Nombre de usuario de MySQL
+define('PASSWORD', ''); // Contraseña de MySQL
+define('DATABASE', 'jesuitas'); // Nombre de la base de datos
